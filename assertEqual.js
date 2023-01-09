04552a4db0746246d1e7c7dc7fcc10e62b60c822
RJ <randy.j416@yahoo.com>
2023-01-09 15:34:00 -0500

    assertEqual created

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀😀Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🙃🙃Assertion Failed: " + actual + " !== " + expected);
+  }
+
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual("YANKEES", "YANKEES");
+assertEqual(12, 125);
