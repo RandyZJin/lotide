48bc4dd4bc5bab09ce9d1a318e3733794d837e8a
RJ <randy.j416@yahoo.com>
2023-01-09 15:45:10 -0500

    head.js

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀😀Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙃🙃Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+
+};
+
+const head = function(array) {
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
