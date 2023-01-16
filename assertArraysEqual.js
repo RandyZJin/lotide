ad49a496b0c15d7ab82aa4966ca9b3b08acbcfc9
RJ <randy.j416@yahoo.com>
2023-01-16 13:57:40 -0500

    dried assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,7 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const assertEqual = require('./assertEqual');
 
-// TEST CODE
+const eqArrays = require('./eqArrays');
+
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2) === true) {
@@ -19,5 +10,11 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`${array1} and ${array2} are not identical!`);
   }
 };
+
+module.exports = assertArraysEqual;
+
+// TEST CODE
+
+
 
 
